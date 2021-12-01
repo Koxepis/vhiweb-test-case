6b2c441382a2f9ae9e8020f93a79aa2fd79aaa22
Shar51 <andiasar11@gmail.com>
2021-12-01 18:33:14 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@vechaiui/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@vechaiui/core"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+  ],
+};
