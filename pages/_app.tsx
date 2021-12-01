1a18ce2bd32da06f8d7f30fbfa4626f2424e310e
Shar51 <andiasar11@gmail.com>
2021-12-01 18:33:00 +0700

    initiate bee colorScheme for VechaiProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,47 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import type { AppProps } from "next/app";
+import {
+  ColorScheme,
+  colors,
+  extendTheme,
+  VechaiTheme,
+  VechaiProvider,
+} from "@vechaiui/react";
+
+const bee: ColorScheme = {
+  id: "bee",
+  type: "light",
+  colors: {
+    bg: {
+      base: colors.warmGray["100"],
+      fill: colors.warmGray["100"],
+    },
+    text: {
+      foreground: colors.warmGray["900"],
+      muted: colors.warmGray["700"],
+    },
+    primary: colors.amber,
+    neutral: colors.warmGray,
+  },
+};
+
+const theme: VechaiTheme = extendTheme({
+  cursor: "pointer",
+  rounded: "0.25rem",
+  colorSchemes: {
+    bee,
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <VechaiProvider theme={theme} colorScheme="bee">
+        <Component {...pageProps} />
+      </VechaiProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
