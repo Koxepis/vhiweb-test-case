bfe2e4146c0cc049738ad7385617659e0a6080a7
Shar51 <andiasar11@gmail.com>
2021-12-02 20:16:57 +0700

    fix: route path

--- components/templates/Navbar.tsx
+++ components/templates/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Example() {
                   </a>
 
                   <a
-                    href="./users"
+                    href="../users/"
                     className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                   >
                     User List
