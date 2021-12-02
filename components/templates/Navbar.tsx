f3a38dada2ff36d74ff09a94f9302f77e60751a4
Shar51 <andiasar11@gmail.com>
2021-12-02 19:08:42 +0700

    remove unused modules
    remove unused declaration

--- components/templates/Navbar.tsx
+++ components/templates/Navbar.tsx
@@ -1,11 +1,5 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Disclosure } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
-
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(" ");
-}
 
 export default function Example() {
   return (
@@ -15,7 +9,6 @@ export default function Example() {
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button */}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primay-500">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -36,7 +29,7 @@ export default function Example() {
                   </a>
 
                   <a
-                    href="./"
+                    href="./users"
                     className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                   >
                     User List
