5b8fd1773df6fcddf8919bb77c9e5f53bd591f5a
Shar51 <andiasar11@gmail.com>
2021-12-03 08:46:19 +0700

    implementing next/router

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    router.push("/users");
+  };
+
   return (
     <>
-      <div className="w-10 h-52 bg-red-900">Test</div>
+      <div className="flex items-center justify-center  min-h-[50vh] sm:min-h-screen">
+        <button
+          onClick={handleSubmit}
+          className="bg-primary-500 p-1.5 px-3 rounded-md border border-neutral-600"
+        >
+          submit
+        </button>
+      </div>
     </>
   );
 };
