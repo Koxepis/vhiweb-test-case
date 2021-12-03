c087851b28f7c83b463ec0172b070d975bbd101b
Shar51 <andiasar11@gmail.com>
2021-12-03 16:06:38 +0700

    change export components

--- shared/helpers/supabaseClient.ts
+++ shared/helpers/supabaseClient.ts
@@ -4,14 +4,10 @@ import {
 } from "./../utils/contants";
 import { createClient } from "@supabase/supabase-js";
 
-if (!NEXT_PUBLIC_SUPABASE_URL)
-  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
+if (!NEXT_PUBLIC_SUPABASE_URL) throw new Error("Missing env.NEXT_APP_SUPABASE_URL");
 if (!NEXT_PUBLIC_SUPABASE_ANON_KEY)
-  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY");
+  throw new Error("Missing env.NEXT_APP_SUPABASE_ANON_KEY");
 
-const supabase = createClient(
+export const supabase = createClient(
   NEXT_PUBLIC_SUPABASE_URL,
-  NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
-
-export default supabase;
+  NEXT_PUBLIC_SUPABASE_ANON_KEY);
