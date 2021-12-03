dd9fa48facfba2d2483c11cb7dfc25f2c094ba47
Shar51 <andiasar11@gmail.com>
2021-12-03 09:49:20 +0700

    initial commit

---
+++ shared/helpers/supabaseClient.ts
@@ -0,0 +1,13 @@
+import { NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY } from './../utils/contants';
+import { createClient } from "@supabase/supabase-js";
+
+if (!NEXT_PUBLIC_SUPABASE_URL) throw new Error("Missing env.NEXT_APP_SUPABASE_URL");
+if (!NEXT_PUBLIC_SUPABASE_ANON_KEY)
+  throw new Error("Missing env.NEXT_APP_SUPABASE_ANON_KEY");
+
+const supabase = createClient(
+  NEXT_PUBLIC_SUPABASE_URL,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
+export default supabase;
