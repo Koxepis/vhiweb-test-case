8d56fa89c80f5f32cbb08c91295eefc6f8ad8aa2
Shar51 <andiasar11@gmail.com>
2021-12-04 00:24:16 +0700

    implementing nextjs scss module

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,50 +1,42 @@
-import { useRouter } from 'next/router';
-import { useState } from 'react';
-import { supabase } from '../shared/helpers/supabaseClient';
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { supabase } from "../shared/helpers/supabaseClient";
+import styles from "../styles/auth.module.scss";
 
 const Auth: React.FC = () => {
-	const [
-		email,
-		setEmail
-	] = useState('');
-	const router = useRouter();
+  const [email, setEmail] = useState("");
+  const router = useRouter();
 
-	const handleSubmit = async (e: React.FormEvent) => {
-		e.preventDefault();
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-		const { error } = await supabase.auth.signIn({ email });
+    const { error } = await supabase.auth.signIn({ email });
 
-		if (!error) {
-			router.push('/users');
-		} else {
-			router.push('/reject');
-		}
-		console.log({ error });
-	};
+    if (!error) {
+      router.push("/users");
+    } else {
+      router.push("/reject");
+    }
+    console.log({ error });
+  };
 
-	return (
-		<div className="bg-white border rounded-lg p-6 sm:p-12 w-4/5 sm:w-4/12 mx-auto my-48">
-			<h3 className="font-extrabold text-3xl">Welcome</h3>
+  return (
+    <div className={styles.container}>
+      <h3>Welcome</h3>
 
-			<p className="text-gray-500 text-sm mt-4">{`Fill in your email, we'll send you a magic link.`}</p>
+      <p>{`Fill in your email, we'll send you a magic link.`}</p>
 
-			<form onSubmit={handleSubmit}>
-				<input
-					type="email"
-					placeholder="Your email address"
-					className="border w-full sm:p-3 rounded-lg mt-2 sm:mt-4 focus:border-primary-500"
-					onChange={(e) => setEmail(e.target.value)}
-				/>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Your email address"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-				<button
-					type="submit"
-					className="bg-primary-500 text-white w-full p-3 rounded-lg mt-2 sm:mt-8 hover:bg-primary-700"
-				>
-					Submit
-				</button>
-			</form>
-		</div>
-	);
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Auth;
