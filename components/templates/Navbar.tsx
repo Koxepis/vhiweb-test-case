0fadea9b4a24976bc66218a3805d8d84212f864a
Shar51 <andiasar11@gmail.com>
2021-12-03 13:13:14 +0700

    fix: style for active component

--- components/templates/Navbar.tsx
+++ components/templates/Navbar.tsx
@@ -22,15 +22,15 @@ export default function Example() {
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
                   <a
-                    href="./"
-                    className="border-primary-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    href="./auth"
+                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                   >
                     Home
                   </a>
 
                   <a
                     href="../users/"
-                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    className="border-primary-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                   >
                     User List
                   </a>
@@ -43,14 +43,14 @@ export default function Example() {
             <div className="pt-2 pb-4 space-y-1">
               <a
                 href="./"
-                className="bg-primary-50 border-primary-500 text-primary-700 block pl-3 pr-4 py-2 border-l-4 text-sm font-medium"
+                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-sm font-medium"
               >
                 Home
               </a>
 
               <a
                 href="./"
-                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-sm font-medium"
+                className="bg-primary-50 border-primary-500 text-primary-700 block pl-3 pr-4 py-2 border-l-4 text-sm font-medium"
               >
                 User List
               </a>
