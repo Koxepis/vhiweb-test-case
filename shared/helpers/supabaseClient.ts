49320e0e90ff0dce90e43ef501581e01004016fa
Shar51 <andiasar11@gmail.com>
2021-12-03 18:25:50 +0700

    declare local env variable

--- shared/helpers/supabaseClient.ts
+++ shared/helpers/supabaseClient.ts
@@ -1,13 +1,9 @@
-import {
-  NEXT_PUBLIC_SUPABASE_URL,
-  NEXT_PUBLIC_SUPABASE_ANON_KEY,
-} from "./../utils/contants";
 import { createClient } from "@supabase/supabase-js";
 
-if (!NEXT_PUBLIC_SUPABASE_URL) throw new Error("Missing env.NEXT_APP_SUPABASE_URL");
-if (!NEXT_PUBLIC_SUPABASE_ANON_KEY)
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL) throw new Error("Missing env.NEXT_APP_SUPABASE_URL");
+if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
   throw new Error("Missing env.NEXT_APP_SUPABASE_ANON_KEY");
 
 export const supabase = createClient(
-  NEXT_PUBLIC_SUPABASE_URL,
-  NEXT_PUBLIC_SUPABASE_ANON_KEY);
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
