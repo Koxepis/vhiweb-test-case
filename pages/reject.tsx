99f2cc0182c583fd695d37ca76738a114ada4ff8
Shar51 <andiasar11@gmail.com>
2021-12-03 13:12:45 +0700

    add style

--- pages/reject.tsx
+++ pages/reject.tsx
@@ -1,7 +1,23 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 const Reject = (props: {}) => {
-  return <div>You rejected</div>;
+  const router = useRouter();
+
+  setTimeout(() => {
+    router.push("/auth");
+  }, 2000);
+
+  return (
+    <div className="w-4/5 sm:w-4/12 my-48 bg-white flex flex-col justify-center items-center shadow-sm mx-auto py-12">
+      <h1 className="font-extrabold text-2xl text-center">
+        {"You're Rejected"}
+      </h1>
+      <p className="text-sm sm:text-base text-gray-700">
+        Please sign in first to continue
+      </p>
+    </div>
+  );
 };
 
 export default Reject;
